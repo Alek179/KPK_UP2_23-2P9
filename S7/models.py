552581146f866d17a66dd14f2f77a5a0ad49a5e8
CatJAM223 <vanasutagin47@gmail.com>
2026-03-30 17:43:32 +0300

    Add files via upload

--- S7/models.py
+++ S7/models.py
@@ -1,4 +1,5 @@
 from peewee import *
+from datetime import date, datetime
 
 db = SqliteDatabase('S7.db')
 
@@ -7,15 +8,37 @@ class BaseModel(Model):
         database = db
 
 class Group(BaseModel):
-    id = IntegerField(primary_key=True)
     year_create = IntegerField()
     number = IntegerField()
     prefix = CharField()
     code = CharField()
     class_number = IntegerField()
-    id_tutor = IntegerField()
-    status = BooleanField()
-    count_student = IntegerField()
+    tutor_id = IntegerField(default=None, null=True)
+    is_active = BooleanField(default=True)
+    count_student = IntegerField(default=0)
+
+
+    @staticmethod
+    def get_course_number(admission_year):
+        current_date = date.today()
+        current_year = current_date.year
+        current_month = current_date.month
+        
+        if current_month >= 9:
+            current_academic_year = current_year
+        else:
+            current_academic_year = current_year - 1
+        if admission_year > current_academic_year:
+            return None
+        
+        course = current_academic_year - admission_year + 1
+        
+        return course
+
+    @property
+    def name(self) -> str:
+        course = Group.get_course_number(self.year_create)
+        return f"{course}-{self.number}{self.prefix}{self.class_number}"
 
 class Student(BaseModel):
     id_student = IntegerField()
@@ -24,4 +47,5 @@ class Student(BaseModel):
 def createTable():
     db.create_tables([Group, Student])
 
-createTable()
+if __name__ == '__main__':
+    createTable()
